6ae58276cc93b73ed34ef8b31007d042e422acdc
amandell2 <amanda.mandell2@gmail.com>
2022-03-17 18:03:39 -0400

    custom pizza form and submission added

--- src/routes/pizza.ts
+++ src/routes/pizza.ts
@@ -39,4 +39,43 @@ routes.get("/confirmation", (req, res) => {
     });
 })
 
+routes.get("/custom-pizza-create", (req, res) => {
+    const size = req.query.size as string;
+    const toppings = Number(req.query.toppings);
+    const glutenFree: boolean = !!req.query.crust;
+    const instructions = req.query.instructions as string;
+    res.render('custom-pizza-create',{
+        size: size,
+        toppings: toppings,
+        crust: glutenFree,
+        instructions: instructions
+    });
+})
+
+routes.get("/custom-pizza-submit", (req, res) => {
+    const size = req.query.size as string;
+    const toppings: number = Number(req.query.toppings);
+    const glutenFree: boolean = !!req.query.crust;
+    const instructions = req.query.instructions as string;
+    let price = 0;
+    if(size === "small"){
+       price += 7+(toppings*0.5) 
+    }else if(size=== "medium"){
+        price += 10+(toppings*1)
+    }else if(size === "large"){
+        price += 12+(toppings*1.25)
+    };
+    if(glutenFree){
+        price += 2
+    }
+    let total = price.toFixed(2);
+    res.render('custom-pizza-submit',{
+        size: size,
+        toppings: toppings,
+        crust: glutenFree,
+        instructions: instructions,
+        price: total
+    });
+})
+
 export default routes; 
