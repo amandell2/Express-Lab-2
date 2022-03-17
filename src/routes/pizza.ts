3d5932ed399208456e35688975f07216b7490b84
amandell2 <amanda.mandell2@gmail.com>
2022-03-16 23:00:55 -0400

    updated ts file with render

--- src/routes/pizza.ts
+++ src/routes/pizza.ts
@@ -8,14 +8,35 @@ routes.get('/', (req, res) => {
     res.render('homepage');
 });
 
-routes.get("/specialty-pizza", (req, res) => {
-    
-})
+routes.get('/specialty-pizza', (req, res) => {
+    const pizzaName: string = req.query.name as string;
+    const price: string = req.query.price as string;
+    res.render('specialty-pizza', {
+        name: pizzaName,
+        price: price
+    });
+});
 
 routes.get("/review", (req, res) => {
     const name = req.query.name as string;
     const comment = req.query.comment as string;
     const rating = req.query.rating as string;
+    res.render('review', {
+        name: name,
+        comment: comment,
+        rating: rating
+    });
+})
+
+routes.get("/confirmation", (req, res) => {
+    const name = req.query.name as string;
+    const comment = req.query.comment as string;
+    const rating = req.query.rating as string;
+    res.render('confirmation', {
+        name: name,
+        comment: comment,
+        rating: rating
+    });
 })
 
 export default routes; 
